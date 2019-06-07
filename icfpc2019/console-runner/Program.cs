d9302462736710d3ce234c0c0f0bf58c55119b53
Alexey Kirpichnikov <alex.kirp@gmail.com>
2019-06-07 16:25:56 +0500

    Add MongoDB example application

---
+++ icfpc2019/console-runner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace console_runner
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var client = new MongoClient("mongodb://icfpc19-mongo1:27017");
+            var database = client.GetDatabase("icfpc");
+            var collection = database.GetCollection<BsonDocument>("tmp");
+            
+            var document = new BsonDocument
+            {
+                { "name", "SomeDocument" },
+            };
+            await collection.InsertOneAsync(document);
+            
+            await collection.Find(new BsonDocument()).ForEachAsync(d => Console.WriteLine(d));
+
+            await database.DropCollectionAsync("tmp");
+        }
+    }
+}
