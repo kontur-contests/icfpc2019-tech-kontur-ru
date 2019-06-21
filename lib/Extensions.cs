c5fc56e82aa7b79bfa57c0078710b0f10539f9c0
Pavel Egorov <xoposhiy@mail.ru>
2019-06-21 16:46:10 +0500

    Rotate V around zero

---
+++ lib/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using lib.Models;
+
+namespace lib
+{
+    public static class Extensions
+    {
+        public static V RotateAroundZero(this V p, bool clockwise)
+        {
+            return clockwise 
+                ? new V(p.Y, -p.X) 
+                : new V(-p.Y, p.X);
+        }
+    }
+}
---
+++ lib/V.cs
@@ -0,0 +1,38 @@
+﻿namespace lib
+{
+    public class V
+    {
+        public V(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        protected bool Equals(V other) => X == other.X && Y == other.Y;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((V)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+
+        public override string ToString() 
+            => $"({X},{Y})";
+
+        public int X { get; }
+        public int Y { get; }
+    }
+}
