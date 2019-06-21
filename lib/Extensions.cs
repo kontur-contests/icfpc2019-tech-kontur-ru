50738f79d92ef4d69a4b590bca650c8255ae16b8
Pavel Egorov <xoposhiy@mail.ru>
2019-06-21 17:34:21 +0500

    GetOrAdd

--- lib/Extensions.cs
+++ lib/Extensions.cs
@@ -13,5 +13,25 @@ namespace lib
                 ? new V(p.Y, -p.X) 
                 : new V(-p.Y, p.X);
         }
+
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, Func<TKey, TValue> createIfNone)
+        {
+            if (dict.TryGetValue(key, out var value))
+                return value;
+            var newValue = createIfNone(key);
+            dict.Add(key, newValue);
+            return newValue;
+        }
+
+        public static bool IsReachable(this V src, V dest, Map map)
+        {
+            var dx = dest.X - src.X;
+            var dy = dest.Y - src.Y;
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+            }
+            throw new NotImplementedException();
+        }
+
     }
 }
