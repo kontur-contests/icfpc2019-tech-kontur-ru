cc790173abe40e4f642e745630ced6e2ddb44956
Pavel Egorov <xoposhiy@mail.ru>
2019-06-21 21:09:42 +0500

    fix Worker.GetManipulators

--- tests/WorkerTests.cs
+++ tests/WorkerTests.cs
@@ -23,7 +23,24 @@ namespace tests
                 }
             };
             var points = worker.GetManipulators(Direction.Left);
-            points.Should().BeEquivalentTo(new V[] {new V(-1, 1), new V(-1, -1), new V(-1, 1),});
+            points.Should().BeEquivalentTo(new V[] {new V(-1, 1), new V(-1, -1), new V(-1, 0),});
+        }
+
+        [TestCase("1,1", Direction.Left, Direction.Left, "1,1")]
+        [TestCase("2,1", Direction.Up, Direction.Right, "1,-2")]
+        [TestCase("2,1", Direction.Up, Direction.Left, "-1,2")]
+        public void GetManipulators(string initial, Direction initialDir, Direction finalDirection, string expected)
+        {
+            var worker = new Worker
+            {
+                Direction = initialDir,
+                Manipulators = new List<V>
+                {
+                    initial,
+                }
+            };
+            var points = worker.GetManipulators(finalDirection);
+            points.Should().BeEquivalentTo(new V[] {expected});
         }
     }
 }
