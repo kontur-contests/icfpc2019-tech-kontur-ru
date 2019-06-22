3b672b256ce84c29e926aa1917a61504ee7bf7a2
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-22 14:56:52 +0500

    StupidSolver

--- lib/Solvers/StupidSolver.cs
+++ lib/Solvers/StupidSolver.cs
@@ -10,7 +10,8 @@ namespace lib.Solvers
 {
     public class StupidSolver : ISolver
     {
-        private List<ActionBase> result;
+        private List<List<ActionBase>> result;
+        private Random random = new Random();
 
         public string GetName()
         {
@@ -19,60 +20,71 @@ namespace lib.Solvers
 
         public int GetVersion()
         {
-            return 1;
+            return 3;
         }
 
         public List<List<ActionBase>> Solve(State state)
         {
-            result = new List<ActionBase>();
+            result = new List<List<ActionBase>> { new List<ActionBase>() };
 
-            BoosterMaster.CreatePalka(state, result);
+            //BoosterMaster.CreatePalka(state, result[0]);
 
-            while (true)
+            BoosterMaster.CloneAttack(state, result);
+
+            while (state.UnwrappedLeft > 0)
             {
-                var map = state.Map;
-                var me = state.SingleWorker;
+                var workerActions = new List<(Worker worker, ActionBase action)>();
 
-                var pathBuilder = new PathBuilder(map, me.Position, true);
-
-                V best = null;
-                var bestDist = int.MaxValue;
-
-                for (int x = 0; x < map.SizeX; x++)
-                for (int y = 0; y < map.SizeY; y++)
+                for (int i = 0; i < state.Workers.Count; i++)
                 {
-                    if (map[new V(x, y)] != CellState.Void)
-                        continue;
+                    var map = state.Map;
+                    var me = state.Workers[i];
 
-                    var dist = pathBuilder.Distance(new V(x, y));
-                    if (dist < bestDist)
+                    var pathBuilder = new PathBuilder(map, me.Position, state.Workers.Take(i).Select(w => w.Position).ToList());
+
+                    V best = null;
+                    var bestDist = int.MaxValue;
+
+                    for (int x = 0; x < map.SizeX; x++)
+                    for (int y = 0; y < map.SizeY; y++)
                     {
-                        bestDist = dist;
-                        best = new V(x, y);
+                        if (map[new V(x, y)] != CellState.Void)
+                            continue;
+
+                        var dist = pathBuilder.Distance(new V(x, y));
+                        if (dist == int.MaxValue)
+                            continue;
+                        
+                        if (dist < bestDist)
+                        {
+                            bestDist = dist;
+                            best = new V(x, y);
+                        }
                     }
+                    
+                    var action = best == null ? new Wait() : pathBuilder.GetActions(best).First();
+                    workerActions.Add((me, action));
+                    result[i].Add(action);
                 }
 
-                if (best == null)
-                    break;
-
-                var actions = pathBuilder.GetActions(best).Take(1).ToList();
-                state.ApplyRange(actions);
-                result.AddRange(actions);
+                state.Apply(workerActions);
             }
 
-            return new List<List<ActionBase>> {result};
+            return result;
         }
 
         private class PathBuilder
         {
             private readonly V start;
+            private readonly List<V> other;
             private Queue<V> queue;
             private Map<int> distance;
             private Map<V> parent;
 
-            public PathBuilder(Map map, V start, bool stop)
+            public PathBuilder(Map map, V start, List<V> other)
             {
                 this.start = start;
+                this.other = other;
                 queue = new Queue<V>();
                 queue.Enqueue(start);
 
@@ -82,7 +94,7 @@ namespace lib.Solvers
                 while (queue.Any())
                 {
                     var v = queue.Dequeue();
-                    if (map[v] == CellState.Void && stop)
+                    if (map[v] == CellState.Void && !TooClose(v))
                         break;
 
                     for (var direction = 0; direction < 4; direction++)
@@ -98,7 +110,7 @@ namespace lib.Solvers
                 }
             }
 
-            public int Distance(V v) => parent[v] == null ? int.MaxValue : distance[v];
+            public int Distance(V v) => parent[v] == null || TooClose(v) ? int.MaxValue : distance[v];
 
             public List<ActionBase> GetActions(V to)
             {
@@ -115,6 +127,11 @@ namespace lib.Solvers
 
                 result.Reverse();
                 return result;
+            }
+
+            private bool TooClose(V v)
+            {
+                return other.Any(o => (o - v).MLen() < 3);
             }
         }
     }
