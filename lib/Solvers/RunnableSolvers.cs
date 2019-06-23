04b16a820bd68b111dbd381812d3d41ad155ac42
Pavel Egorov <xoposhiy@mail.ru>
2019-06-23 14:08:55 +0500

    prepare to crunch

--- lib/Solvers/RunnableSolvers.cs
+++ lib/Solvers/RunnableSolvers.cs
@@ -28,7 +28,7 @@ namespace lib.Solvers
         {
             return new List<Func<ISolver>>
             {
-                //() => new RandomWalkSolver(depth: 2, new Estimator(true), new Random(Guid.NewGuid().GetHashCode()), 100, usePalka: true, true),
+                () => new RandomWalkSolver(depth: 2, new Estimator(true), new Random(Guid.NewGuid().GetHashCode()), 100, usePalka: true, true),
                 () => new DeepWalkSolver(depth: 2, new Estimator(true), true, true),
                 //() => new StupidSolver(),
                 //() => new RandomWalkSolver(depth: 2, new Estimator(), new Random(Guid.NewGuid().GetHashCode()), 100, usePalka: true),
